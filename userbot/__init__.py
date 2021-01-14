2f8252ef20066881f124af0ab81117bd5e8422f5
Javes786 <73664034+Javes786@users.noreply.github.com>
2021-01-14 16:46:21 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -15,10 +15,10 @@ skyversion = "2.0"
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), var.APP_ID, var.API_HASH)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
-    bot = TelegramClient(session_name, var.APP_ID, var.API_HASH)
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
 
 CMD_LIST = {}
