fb344def29f179503b3c5267ae30a916c0cd86cb
SKYLORD <76394118+skylord-op@users.noreply.github.com>
2021-01-15 11:15:45 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -14,9 +14,6 @@ from pathlib import Path
 from time import gmtime, strftime
 
 from telethon import events
-from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
-
 from userbot import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from userbot.helper.exceptions import CancelProcess
 from var import Var
