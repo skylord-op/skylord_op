2c0073218e39ded4c1e0011d6033a2b30079c034
SKYLORD <76394118+skylord-op@users.noreply.github.com>
2021-01-15 08:44:15 +0530

    Update function.py

--- userbot/helper/function.py
+++ userbot/helper/function.py
@@ -1,7 +1,3 @@
-# Making it easy....
-# thanks to @ranger_op for idea
-# codes by @mrconfused
-# catuserbot
 
 import os
 
@@ -235,12 +231,12 @@ async def solarize(imagefile, endname):
 
 async def iphonex(text):
     r = requests.get(f"https://nekobot.xyz/api/imagegen?type=iphonex&url={text}").json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png").convert("RGB")
     img.save("temp.jpg", "jpeg")
     return "temp.jpg"
@@ -250,12 +246,12 @@ async def baguette(text):
     r = requests.get(
         f"https://nekobot.xyz/api/imagegen?type=baguette&url={text}"
     ).json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png").convert("RGB")
     img.save("temp.jpg", "jpeg")
     return "temp.jpg"
@@ -263,12 +259,12 @@ async def baguette(text):
 
 async def threats(text):
     r = requests.get(f"https://nekobot.xyz/api/imagegen?type=threats&url={text}").json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
@@ -278,12 +274,12 @@ async def threats(text):
 
 async def lolice(text):
     r = requests.get(f"https://nekobot.xyz/api/imagegen?type=lolice&url={text}").json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
@@ -293,12 +289,12 @@ async def lolice(text):
 
 async def trash(text):
     r = requests.get(f"https://nekobot.xyz/api/imagegen?type=trash&url={text}").json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
@@ -308,12 +304,12 @@ async def trash(text):
 
 async def awooify(text):
     r = requests.get(f"https://nekobot.xyz/api/imagegen?type=awooify&url={text}").json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
@@ -325,12 +321,12 @@ async def trap(text1, text2, text3):
     r = requests.get(
         f"https://nekobot.xyz/api/imagegen?type=trap&name={text1}&author={text2}&image={text3}"
     ).json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
@@ -342,12 +338,12 @@ async def phcomment(text1, text2, text3):
     r = requests.get(
         f"https://nekobot.xyz/api/imagegen?type=phcomment&image={text1}&text={text2}&username={text3}"
     ).json()
-    kraken = r.get("message")
-    hellurl = url(kraken)
-    if not hellurl:
+    skylord = r.get("message")
+    skyurl = url(skylord)
+    if not skyurl:
         return "check syntax once more"
     with open("temp.png", "wb") as f:
-        f.write(requests.get(kraken).content)
+        f.write(requests.get(skylord).content)
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
