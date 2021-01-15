f50d7c00d890736a894e82737e2edfe65e8c0f3a
ASHU-OP <76394118+skylord-op@users.noreply.github.com>
2021-01-15 20:32:51 +0530

    Update chain.py

--- userbot/plugins/chain.py
+++ userbot/plugins/chain.py
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from telethon.tl.functions.messages import SaveDraftRequest
+from telethon.tl.function.messages import SaveDraftRequest
 
 from userbot import CMD_HELP
 from userbot.utils import admin_cmd, sudo_cmd
