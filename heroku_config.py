f7c76ec8391ecc58fabc50389219a2c0d169972e
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-16 10:30:54 +0530

    Fix

--- heroku_config.py
+++ heroku_config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Var(object):
-    API_ID = int(os.environ.get("API_ID", 6))
+    APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
